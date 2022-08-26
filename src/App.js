7d239709ee96478ff6df5e8ec3214d03aa4d7973
Emilio Conde <econde@npitx.org>
2022-08-26 15:19:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter,faClock,faHeart,faGripHorizontal,faGripVertical,faGear,faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+<div id="bg1">
+      <div id="profileBox">
+        <div id="profileImage"/>
+        <h1><FontAwesomeIcon icon={faFilter} />Filter</h1>
+        
+        <h1><FontAwesomeIcon icon={faClock} />Clock</h1>
+        <h1><FontAwesomeIcon icon={faHeart} />Favorites</h1>
+        <FontAwesomeIcon id="carouselIcons" icon={faGripVertical}></FontAwesomeIcon>
+        <FontAwesomeIcon id="carouselIcons" icon={faGripHorizontal}></FontAwesomeIcon>
+        <div id="profileButton" ><FontAwesomeIcon icon={faHeart}/>Profile</div>
+        <div id="profileButton"><FontAwesomeIcon icon={faGear}/>Options</div>
+        <h1><FontAwesomeIcon icon={faArrowLeft}/>Exit</h1>
+      </div>
+  </div>
+    
+  )
 }
 
-export default App;
+export default App
+
